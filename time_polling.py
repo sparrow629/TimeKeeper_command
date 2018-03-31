a169004e77c749afcf511386c8a427af19d113f2
sparrow <sparrow@SparrowdeMacBook-Pro.local>
2018-03-31 22:58:21 +0800

    add yesterday this_week

--- time_polling.py
+++ time_polling.py
@@ -16,23 +16,39 @@ class TimePolling(object):
 			self.func(date)
 			date = date + datetime.timedelta(days = 1)
 
-	def time_polling_duration(self, duraiton):
+	def time_polling_duration(self, duration):
 		date = datetime.date(*map(int, self.endtime.split('-')))
-		count = duraiton
+		count = duration
 		while not count == 0:
 			self.func(date)
 			date = date - datetime.timedelta(days = 1)
 			count -= 1
 
+	def yesterday(self):
+		yesterday = (datetime.datetime.now() - datetime.timedelta(days = 1)).strftime('%Y-%m-%d')
+		self.func(yesterday)
+
+	def this_week(self):
+		date = datetime.datetime.now()
+		duration = date.weekday() + 1
+		count = duration
+		while not count == 0:
+			self.func(date.strftime('%Y-%m-%d'))
+			date = date - datetime.timedelta(days=1)
+			count -= 1
+
 def printer(date):
 	print date
+
 
 def show():
 	starttime = '2018-02-28'
 	endtime = '2018-03-09'
 	polling = TimePolling(printer, starttime, endtime)
 	# polling.time_polling()
-	polling.time_polling_duration(20)
+	# polling.time_polling_duration(20)
+	# polling.yesterday()
+	polling.this_week()
 
 if __name__ == '__main__':
 	show()
